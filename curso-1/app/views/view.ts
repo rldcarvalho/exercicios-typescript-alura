ae473529e3d8be2b8f974cef446f7caf706fa8ee
rldcarvalho <rldcarvalhoo@gmail.com>
2023-03-22 19:00:56 -0300

    Aplicado herança nas classes views

---
+++ curso-1/app/views/view.ts
@@ -0,0 +1,17 @@
+export class View<T> {
+
+    protected elemento: HTMLElement;
+
+    constructor(seletor: string){
+        this.elemento = document.querySelector('seletor');
+    }
+
+    template(model: T): string {
+        throw Error('A classe filha deve sobrescrever o método template da classe mãe')
+    }
+
+    update(model: T): void {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
+}
---
+++ curso-1/dist/js/views/view.js
@@ -0,0 +1,12 @@
+export class View {
+    constructor(seletor) {
+        this.elemento = document.querySelector('seletor');
+    }
+    template(model) {
+        throw Error('A classe filha deve sobrescrever o método template da classe mãe');
+    }
+    update(model) {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
+}
