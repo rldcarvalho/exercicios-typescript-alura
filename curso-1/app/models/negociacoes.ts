b748ce176d0339abc7a8faebb4591590dd9f5438
rldcarvalho <rldcarvalhoo@gmail.com>
2023-03-23 22:57:01 -0300

    Extraído método atualiza view

--- curso-1/app/models/negociacoes.ts
+++ curso-1/app/models/negociacoes.ts
@@ -3,11 +3,11 @@ import { Negociacao } from "./negociacao.js";
 export class Negociacoes {
     private negociacoes: Array<Negociacao> = []; //negociacoes: Negociacao[]
 
-    adiciona(negociacao: Negociacao){
+    public adiciona(negociacao: Negociacao){
         this.negociacoes.push(negociacao);
     }
 
-    lista(): ReadonlyArray<Negociacao>{ //readonly Negociacao[]
+    public lista(): ReadonlyArray<Negociacao>{ //readonly Negociacao[]
         return this.negociacoes;
     }
 }
