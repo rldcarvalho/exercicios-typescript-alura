4cb007c380b6085f599ff1a9761f877ac9ccaa85
rldcarvalho <rldcarvalhoo@gmail.com>
2023-03-27 18:50:46 -0300

    Adicionado proteção ao template

--- curso-1/dist/js/views/view.js
+++ curso-1/dist/js/views/view.js
@@ -1,9 +1,13 @@
 export class View {
     constructor(seletor) {
+        this.escapar = false;
         this.elemento = document.querySelector(seletor);
     }
     update(model) {
-        const template = this.template(model);
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<\/script/, '');
+        }
         this.elemento.innerHTML = template;
     }
 }
