f24a8e9fa469dcd37a5bc76d7445ca20e5750b96
rldcarvalho <rldcarvalhoo@gmail.com>
2023-03-29 19:18:32 -0300

    Adicionado Decorator tempo de execução

--- curso-1/app/src/controllers/negociacao-controllers.ts
+++ curso-1/app/src/controllers/negociacao-controllers.ts
@@ -1,3 +1,4 @@
+import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 import { DiaDaSemana } from "../enums/dia-da-semana.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
@@ -20,6 +21,7 @@ export class NegociacaoController {
         this.negociacoesView.update(this.negociacoes);
     }
 
+    @logarTempoDeExecucao()
     public adiciona(): void{
         const negociacao = Negociacao.criaDe(this.inputData.value, this.inputQuantidade.value, this.inputValor.value);
 
@@ -31,7 +33,6 @@ export class NegociacaoController {
         this.negociacoes.adiciona(negociacao);
         this.atualizaView();
         this.limparFormulario();
-        
     }
 
     private ehDiaUtil(data: Date) {
