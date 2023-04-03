d181ef5ef6684efa8aa2be9ab61b0b8d0545c984
rldcarvalho <rldcarvalhoo@gmail.com>
2023-04-03 18:44:19 -0300

    Adicionada interface negociações do dia

--- curso-1/app/src/controllers/negociacao-controllers.ts
+++ curso-1/app/src/controllers/negociacao-controllers.ts
@@ -2,6 +2,7 @@ import { domInjector } from "../decorators/dom-injector.js";
 import { inspect } from "../decorators/inspect.js";
 import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
 import { DiaDaSemana } from "../enums/dia-da-semana.js";
+import { NegociacoesDoDia } from "../interfaces/negociacao-do-dia.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { MensagemView } from "../views/mensagem-view.js";
@@ -41,7 +42,7 @@ export class NegociacaoController {
     public importaDados(): void {
         fetch('http://localhost:8080/dados')
             .then(res => res.json() )
-            .then((dados: any[]) => {
+            .then((dados: NegociacoesDoDia[]) => {
                 return dados.map(dado => {
                     return new Negociacao(new Date(), dado.vezes, dado.montante);
                 })
